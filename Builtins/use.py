44adbd968fd36085e7c1d7225d466120ee0d10ce
Victor Lantigua <v.lantigua94@gmail.com>
2022-11-16 07:03:26 -0500

    Create use.py

---
+++ Builtins/use.py
@@ -0,0 +1,32 @@
+import docx
+import PyPDF2
+from pathlib import Path
+
+def ReadDocx(path: str) -> list[str]:
+    '''
+    Reads the docx file from the path and returns a list of the words in the document.
+    '''
+    currDocument = docx.Document(path)
+    words = []
+
+    for paragraph in currDocument.paragraphs:
+        words += paragraph.text.split()
+    
+    return words
+
+
+def ReadPDF(path:str) -> list[str]:
+    '''
+    Reads the pdf file from the path and returns a list of the words in the document.
+    '''
+    words = []
+
+    with open(path, 'rb') as file:
+        pdf = PyPDF2.PdfFileReader(file)
+
+        for i in range(pdf.numPages):
+            currPage = pdf.getPage(i)
+            words += currPage.extractText().split()
+        
+    return words
+
